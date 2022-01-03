51bcfb9b87151e546bdb3b04f852d5f2edfcbb8d
Malitha Liyanage <malitharanu@gmail.com>
2022-01-03 14:59:27 +0530

    connected the services with message controller

--- messages/src/repositories/messages.repository.ts
+++ messages/src/repositories/messages.repository.ts
@@ -2,19 +2,19 @@ import { readFile, writeFile } from "fs/promises";
 
 export class MessagesRepository {
   async findOne (id: string) {
-    const contents = await readFile('../messages/messages.json', 'utf8');
+    const contents = await readFile('messages.json', 'utf8');
     const messages = JSON.parse(contents);
 
     return messages[id];
   }
 
   async findAll() {
-    const contents = await readFile('../messages/messages.json', 'utf8');
+    const contents = await readFile('messages.json', 'utf8');
     return JSON.parse(contents);
   }
 
   async create(content: string) {
-    const contents = await readFile('../messages/messages.json', 'utf8');
+    const contents = await readFile('messages.json', 'utf8');
     const messages = JSON.parse(contents);
 
     const id = Math.floor(Math.random() * 999);
