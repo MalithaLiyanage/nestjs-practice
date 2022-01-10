3016427a7bf360999722bc789aaf63ff3ceba0cd
Malitha Liyanage <malitharanu@gmail.com>
2022-01-10 06:56:18 +0530

    Added and practiced dependency injection between services and modules

---
+++ messages/src/computer/computer.controller.ts
@@ -0,0 +1,20 @@
+import { Controller, Get } from '@nestjs/common';
+import { CpuService } from 'src/cpu/cpu.service';
+import { DiskService } from 'src/disk/disk.service';
+import { ComputerModule } from './computer.module';
+
+@Controller('computer')
+export class ComputerController {
+  constructor(
+    private cpuService: CpuService,
+    private diskService: DiskService
+  ) {}
+
+  @Get()
+  run() {
+    return [
+      this.cpuService.compute(1, 2),
+      this.diskService.getData()
+    ];
+  }
+}
